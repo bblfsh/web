d2c0e39b23911b064385a52b16994def5bf3d9f1
Miguel Molina <hi@mvader.me>
2017-06-20 12:07:54 +0200

    initial bootstrap with create-react-app
    Signed-off-by: Miguel Molina <miguel@erizocosmi.co>

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import 'normalize.css';
+
+const Wrap = styled.div`
+  background-color: papayawhip;
+  color: palevioletred;
+`
+
+class App extends Component {
+  render() {
+    return (
+      <Wrap>Placeholder</Wrap>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css';
+import { injectGlobal } from 'styled-components';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+// eslint-disable-next-line
+injectGlobal`
+  * {
+    box-sizing: border-box;
+  }
+`;
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
