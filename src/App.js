40fc402c435c1f44615e5a20ad325a92a2577088
Miguel Molina <miguel@erizocosmi.co>
2017-06-21 15:32:08 +0200

    implement topbar
    Signed-off-by: Miguel Molina <miguel@erizocosmi.co>

--- src/App.js
+++ src/App.js
@@ -21,24 +21,74 @@ const Content = styled.div`
   display: flex;
   height: 100%;
   flex-direction: row;
+  position: relative;
 `
+
+const initialState = {
+  languages: {
+    auto: { name: '(auto)' },
+    python: {
+      name: 'Python',
+      url: '#',
+    },
+    java: {
+      name: 'Java',
+      url: '#',
+    },
+  },
+  actualLanguage: 'python',
+  loading: false,
+  selectedLanguage: 'auto',
+  code: '',
+  ast: undefined,
+};
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = Object.assign({}, initialState);
+  }
+
+  onLanguageChanged(e) {
+    let selectedLanguage = e.target.value;
+    if (!this.state.languages.hasOwnProperty(selectedLanguage)) {
+      selectedLanguage = 'auto';
+    }
+    this.setState({ selectedLanguage });
+  }
+
+  onRunParser(e) {
+    throw new Error('run parser not implemented yet!');
   }
 
   render() {
     const { innerWidth: width } = window;
+    const { languages, selectedLanguage, code, ast, loading, actualLanguage } = this.state;
 
     return (
       <Wrap>
-        <Header />
+        <Header
+          languages={languages}
+          selectedLanguage={selectedLanguage}
+          actualLanguage={actualLanguage}
+          onLanguageChanged={e => this.onLanguageChanged(e)}
+          onRunParser={e => this.onRunParser(e)}
+          loading={loading} />
+
         <Content>
-          <SplitPane split='vertical' minSize={width * 0.25} defaultSize='50%' maxSize={width * 0.75}>
-            <Editor />
-            <UASTViewer />
+          <SplitPane 
+            split='vertical' 
+            minSize={width * 0.25} 
+            defaultSize='50%' 
+            maxSize={width * 0.75}>
+
+            <Editor
+              code={code}
+              loading={loading} />
+
+            <UASTViewer 
+              ast={ast}
+              loading={loading} />
           </SplitPane>
         </Content>
       </Wrap>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,134 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import bblfshLogo from '../img/babelfish_logo.svg';
+import githubIcon from '../img/github.svg';
+
 const Container = styled.header`
   height: 70px;
+  padding: 0 1rem;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid black;
 `
 
-export default function Header() {
+const Title = styled.h1`
+  display: flex;
+  align-items: center;
+`
+
+const TitleImage = styled.img`
+  height: 40px;
+`
+
+const DashboardTitle = styled.span`
+  font-weight: bold;
+  font-size: 1.5rem;
+  margin-left: .5rem;
+`
+
+const Actions = styled.div`
+  width: 100%;
+  display: flex;
+  margin-left: 1rem;
+`
+
+const InputGroup = styled.div`
+  display: flex;
+  align-items: center;
+
+  & + & {
+    margin-left: 1rem;
+  }
+
+  & > label {
+    margin-right: .5rem;
+  }
+`
+
+const RunButton = styled.button`
+  padding: .2rem 1.5rem;
+  border-radius: 3px;
+  border: none;
+  cursor: pointer;
+`
+
+const DriverCodeBox = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: .5rem;
+`
+
+const DriverCodeText = styled.a`
+  margin-left: .5rem;
+`
+
+export function DriverCode({ languages, selectedLanguage, actualLanguage }) {
+  const driver = selectedLanguage === 'auto' 
+    ? actualLanguage
+    : selectedLanguage;
+
+  return (
+    <DriverCodeBox>
+      <img src={githubIcon} alt='GitHub' />
+      <DriverCodeText 
+        href={languages[driver].url} 
+        target='_blank'>
+        Driver Code
+      </DriverCodeText>
+    </DriverCodeBox>
+  );
+}
+
+export default function Header({
+  selectedLanguage, 
+  languages, 
+  onLanguageChanged,
+  onRunParser,
+  loading,
+  actualLanguage,
+}) {
+  const languageOptions = Object.keys(languages)
+    .map(k => {
+      const name = k === 'auto' 
+        ? `${languages[actualLanguage].name} ${languages[k].name}`
+        : languages[k].name;
+      return (
+        <option value={k} key={k}>{name}</option>
+      );
+    });
+
   return (
     <Container>
-      Babelfish Dashboard
+      <Title>
+        <TitleImage src={bblfshLogo} alt='bblfsh' />
+        <DashboardTitle>Dashboard</DashboardTitle>
+      </Title>
+
+      <Actions>
+        <InputGroup>
+          <label>Language:</label>
+          <select 
+            onChange={onLanguageChanged}
+            value={selectedLanguage}>
+            {languageOptions}
+          </select>
+
+          <DriverCode 
+            languages={languages}
+            selectedLanguage={selectedLanguage}
+            actualLanguage={actualLanguage} />
+        </InputGroup>
+
+        <InputGroup>
+          <RunButton 
+            id='run-parser'
+            onClick={onRunParser}
+            disabled={loading}>
+            Run parser
+          </RunButton>
+        </InputGroup>
+      </Actions>
     </Container>
   );
 }
