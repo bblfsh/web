99cda74a79ee7e4899dda44ed136bc2ef597be7d
Miguel Molina <miguel@erizocosmi.co>
2017-07-06 12:55:32 +0200

    add footer to the page
    Signed-off-by: Miguel Molina <miguel@erizocosmi.co>

--- src/styling/variables.js
+++ src/styling/variables.js
@@ -1,22 +1,24 @@
 const color = {
   accent: {
     deepOrange: '#EB7422',
-    lightOrange: '#F2A83F'
+    lightOrange: '#F2A83F',
   },
   base: {
     darkGrey: '#212121',
     lightGrey: '#B1B1B1',
     white: '#FFFFFF',
-    black: '#0b0b0b'
+    black: '#0b0b0b',
+    light: '#f9f7f5',
   },
   custom: {
-    smoothGrey: '#E1E1E1',
-    highlight: '#FFFF00'
-  }
+    smoothGrey: '#dad7d0',
+    highlight: '#FFFF00',
+  },
 };
 
 const fontSizeHTML = 16;
-const px2rem = px => (px / fontSizeHTML) + 'rem'
+const px2rem = px => (px / fontSizeHTML) + 'rem';
+
 export const font = {
   size: {
     html: fontSizeHTML+'px',
@@ -27,28 +29,36 @@ export const font = {
   },
   lineHeight: {
     large: '1.45em',
-    regular: '1.2em'
+    regular: '1.2em',
   },
   color: {
     dark: color.base.darkGrey,
     light: color.base.lightGrey,
     accentDark: color.accent.deepOrange,
-    accentLight: color.accent.lightOrange
+    accentLight: color.accent.lightOrange,
   },
   family: {
     logo: 'Century Gothic',
     prose: 'Lato, sans-serif',
-    code: 'Inconsolata'
+    code: 'Inconsolata',
   }
 };
 
 export const background = {
   main: color.base.white,
   dark: color.base.black,
-  highlight: color.custom.highlight
+  highlight: color.custom.highlight,
+  light: color.base.light,
+  accent: color.accent.deepOrange,
 };
 
 export const border = {
   light: color.base.lightGrey,
-  smooth: color.custom.smoothGrey
+  smooth: color.custom.smoothGrey,
+  accent: color.accent.deepOrange,
+};
+
+export const shadow = {
+  topbar: 'rgba(138, 128, 115, 0.14)',
+  primaryButton: 'rgba(191, 76, 50, 0.48)',
 };
