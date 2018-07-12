a64708335e76e3442efaa33ec407d2d20006c4e4
Maxim Sukharev <maxim@sourced.tech>
2018-07-12 14:51:33 +0200

    update usage of new enzyme
    Signed-off-by: Maxim Sukharev <maxim@sourced.tech>

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,8 @@
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import fetchMock from 'jest-fetch-mock';
+
+configure({ adapter: new Adapter() });
 
 global.fetch = fetchMock;
 
