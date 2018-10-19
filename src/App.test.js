66b0171d58bf68254ca3fd36f02e2f545079da67
Maxim Sukharev <maxim@sourced.tech>
2018-10-19 17:28:18 +0200

    remove env prop from snapshots
    Signed-off-by: Maxim Sukharev <maxim@sourced.tech>

--- src/App.test.js
+++ src/App.test.js
@@ -6,19 +6,29 @@ import toJson from 'enzyme-to-json';
 
 const noop = () => null;
 
+// apparently SplitPane component keeps enviroment information as a prop.
+// and they are different on linux/os/ci/nodejs/...
+const toJsonFixed = wrapper =>
+  toJson(wrapper, {
+    map: json => {
+      delete json.props.prefixer;
+      return json;
+    },
+  });
+
 it('renders without crashing', () => {
   const wrapper = shallow(<App errors={[]} code="foo = 1" init={noop} />);
-  expect(toJson(wrapper)).toMatchSnapshot();
+  expect(toJsonFixed(wrapper)).toMatchSnapshot();
 });
 
 it('render while loading', async () => {
   const wrapper = shallow(<App errors={[]} code={null} init={noop} />);
-  expect(toJson(wrapper)).toMatchSnapshot();
+  expect(toJsonFixed(wrapper)).toMatchSnapshot();
 });
 
 it('render with errors', async () => {
   const wrapper = shallow(
     <App errors={['error1', 'error2']} code="foo = 1" init={noop} />
   );
-  expect(toJson(wrapper)).toMatchSnapshot();
+  expect(toJsonFixed(wrapper)).toMatchSnapshot();
 });
